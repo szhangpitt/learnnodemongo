2cf78fe1a957cd1b96927d5ed2f935234682af88
Shaopeng Zhang <szhangpitt@gmail.com>
2014-10-29 00:41:29 -0400

    install nodemailer and emailjs

--- learnnodemongo/routes/index.js
+++ learnnodemongo/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Email = require('email');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Jobssss' });
+
+
+	res.render('index', { title: 'Jobssss' });
+
+
 });
 
 router.get('/form', function(req, res) {
@@ -11,6 +16,13 @@ router.get('/form', function(req, res) {
 });
 
 router.get('/about', function(req, res) {
+	console.log(Email);
+	var myMsg = new Email({ from: "me@example.com" , to:   "szhangpitt@gmail.com" , subject: "Knock knock..." , body: "Who's there?"});
+
+	myMsg.send(function(err) {
+		console.log(err);
+	});
+	
 	res.render('index', {title: 'About'});
 });
 
